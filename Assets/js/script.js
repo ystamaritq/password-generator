6a156f45df462ea8d5909bef47dc48df73741f5f
Yadira Tamarit <ystamaritq@gmail.com>
2020-04-30 15:31:19 -0500

    added the control flow if else to validate password length 2 version

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -17,39 +17,44 @@ var uppercase = lowercase.toUpperCase();
 var numeric = "0123456789";
 var specialcharacter = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
+// function Length
 function getPassLength() {
-	// var to save the length of the password
 	var userPassLength = prompt(
 		" Enter the password's length (Number betweem 8 and no more than 128 characters)"
 	);
 
-	if (
-		!(userPassLength === "" && userPassLength >= 8 && userPassLength <= 128)
-	) {
-		alert(
-			"you must to select a valid password length (Number betweem 8 and no more than 128 characters)"
-		);
-	} else {
-		alert("your password will have " + userPassLength + " length");
+	userPassLengthInt = parseInt(userPassLength);
 
-		return userPassLength;
+	if (
+		userPassLengthInt !== "" &&
+		userPassLengthInt >= 8 &&
+		userPassLengthInt <= 128 &&
+		Number.isInteger(userPassLengthInt) === true
+	) {
+		alert("your password will have " + userPassLengthInt + " length");
+		return userPassLengthInt;
+	} else {
+		return -1;
 	}
 }
 
 function generatePassword() {
-	//show in the textarea the value (test)
-	var test = document.getElementById("password-id");
-	test.value = getPassLength();
+	var textvalue = document.getElementById("password-id");
+	textvalue.value = getPassLength();
+	/*
+	if (textvalue > 0) {
+		//confirm window to include in the password : lowerCase
+		var lowerCasePass = confirm("Do you want to include lowercase letters?");
 
-	//confirm window to include in the password : lowerCase
-	var lowerCasePass = confirm("Do you want to include lowercase letters?");
+		//confirm window to include in the password  : upperCase
+		var upperCasePass = confirm("Do you want to include uppercase letters?");
 
-	//confirm window to include in the password  : upperCase
-	var upperCasePass = confirm("Do you want to include uppercase letters?");
+		// confirm window to include in the password: numeric
+		var numericPass = confirm("Do you want to include numbers?");
 
-	// confirm window to include in the password: numeric
-	var numericPass = confirm("Do you want to include numbers?");
-
-	//confirm window to include in the password: characters
-	var charactersPass = confirm("Do you want to include characters?");
+		//confirm window to include in the password: characters
+		var charactersPass = confirm("Do you want to include characters?");
+	} else {
+		alert("test");
+	} */
 }
